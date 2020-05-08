39f2ccb963297ebbf5ffc28caa316a36c4c6f993
Eugene <eugenax2017@gmail.com>
2020-05-08 15:08:14 +0200

    2. The end of sale tokens

--- contracts/GpcTokenSale.sol
+++ contracts/GpcTokenSale.sol
@@ -3,13 +3,39 @@ pragma solidity >=0.4.21 <0.7.0;
 import "./GpcToken.sol";
 
 contract GpcTokenSale {
-    address admin;
+
+    address payable admin;
     GpcToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
 
-    constructor(GpcToken _tokenContract, uint256 _tokenPrice) public {        
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(GpcToken _tokenContract, uint256 _tokenPrice) public payable {        
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+
+        selfdestruct(admin);        
     }
 }
--- test/GpcTokenSale.js
+++ test/GpcTokenSale.js
@@ -1,8 +1,13 @@
 var GpcTokenSale = artifacts.require("./GpcTokenSale.sol");
+var GpcToken = artifacts.require("./GpcToken.sol");
 
 contract('GpcTokenSale', (accounts) => {
     var tokenSaleInstance;
-    var tokenPrice = 1000000000000000; //in wei
+    var admin = accounts[0];
+    var buyer = accounts[1];
+    var tokenPrice = 1000000000000000; //in wei    
+    var tokenAvailable = 750000;
+    var numberOfTokens;
 
     it('initializes the contract with the correct values', () => {
         return GpcTokenSale.deployed().then((instance) => {
@@ -18,4 +23,66 @@ contract('GpcTokenSale', (accounts) => {
             assert.equal(price, tokenPrice, 'token price is correct');
         });
     });
+
+    it('facilitates token buying', () => {
+        return GpcToken.deployed().then((instance) => {
+            //Grab token instance first
+            tokenInstance = instance;            
+            return GpcTokenSale.deployed();
+        }).then((instance) => {
+            //Then grab token sale instance
+            tokenSaleInstance = instance;
+            //Provision 750000 of totalSupply
+            return tokenInstance.transfer(tokenSaleInstance.address, tokenAvailable, { from: admin});
+        }).then((receipt) => {
+            numberOfTokens = 10;   
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice });
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of purchased');
+            return tokenSaleInstance.tokensSold();
+        }).then((amount) => {
+            assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        }).then((balance) => {    
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), tokenAvailable - numberOfTokens);
+            //Try to buy tokens different from the ether value
+             return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more than available');;
+        });    
+    });
+
+    it('ends token sale', function() {
+        return GpcToken.deployed().then((instance) => {            
+            tokenInstance = instance;
+            return GpcTokenSale.deployed();
+        }).then((instance) => {
+            //Then grab token sale instance
+            tokenSaleInstance = instance;
+            //Try to end sale from account other than admin
+            return tokenSaleInstance.endSale({ from: buyer });
+        }).then(assert.fail).catch((error) => {
+            assert(error.message.indexOf('revert') >= 0, 'must be admin to end sale');
+            return tokenSaleInstance.endSale({ from: admin });             
+        }).then((receipt) => {
+            return tokenInstance.balanceOf(admin);
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), 4999990, 'returns all unsold tokens to admin');
+            //Check that token price was reset when selfDestruct was called
+            return tokenSaleInstance.tokenPrice();
+        }).then((price) => {
+            assert.equal(price.toNumber(), 0, 'token price was reset');
+        }).then(assert.fail).catch((error) => {
+
+        });
+    });
+
 });
